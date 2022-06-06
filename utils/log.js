c3cf9dfd43821ead709acf9f9eccc75d884a29f5
Giancarlo Sanchez <gianksp@gmail.com>
2022-06-06 21:05:49 +0100

    Moved to src

--- utils/log.js
+++
@@ -1,8 +0,0 @@
-export class Logger {
-
-    static debug = (message) =>  console.debug(message);
-    static info = (message) =>  console.info(message);
-    static error = (message) =>  console.error(message);
-    static warning = (message) =>  console.warning(message);
-    static log = (message) =>  console.log(message);
-}
