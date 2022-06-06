5090aa9ce17571feae72b8587c4d61252c4858eb
Giancarlo Sanchez <gianksp@gmail.com>
2022-06-06 10:02:38 +0100

    Embedding provider into library

--- DappifyContext.js
+++ DappifyContext.js
@@ -1,14 +1,23 @@
 import PropTypes from 'prop-types';
 import { createContext } from 'react';
 import useDappify from 'react-dappify/hooks/useDappify';
+import { MoralisProvider } from 'react-moralis';
 
 export const DappifyContext = createContext(null);
 
 export const DappifyProvider = ({ children }) => (
+    <MoralisProvider appId={process.env.REACT_APP_MORALIS_APP_ID} serverUrl={process.env.REACT_APP_MORALIS_SERVER_URL}>
+        <DappifyContextProvider>
+            {children}
+        </DappifyContextProvider>
+    </MoralisProvider>
+);
+
+const DappifyContextProvider = ({ children }) => (
     <DappifyContext.Provider value={{ ...useDappify() }}>
         {children}
     </DappifyContext.Provider>
-);
+)
 
 DappifyProvider.propTypes = {
     children: PropTypes.node
